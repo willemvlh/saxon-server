8643f320cb676f399d6b948029bb2dd73c89f092
Willem Van Lishout <willem.van.lishout@mediagenix.tv>
2020-09-07 12:48:33 +0200

    modify actors to run without xml input

--- src/main/java/tv/mediagenix/xslt/transformer/saxon/actors/SaxonXQueryPerformer.java
+++ src/main/java/tv/mediagenix/xslt/transformer/saxon/actors/SaxonXQueryPerformer.java
@@ -22,16 +22,35 @@ public class SaxonXQueryPerformer extends SaxonActor {
         super(config);
     }
 
+    @Override
     public SerializationProperties act(InputStream is, InputStream query, OutputStream output) throws TransformationException {
         try {
-            XQueryEvaluator e = this.getProcessor().newXQueryCompiler().compile(query).load();
-            Serializer s = newSerializer(output);
-            e.setDestination(s);
+            XQueryEvaluator e = newEvaluatorOnQuery(query);
             e.setSource(newSAXSource(is));
-            e.run();
-            return new SerializationProperties("application/xml", "utf-8");
+            return evaluate(e, output);
         } catch (SaxonApiException e) {
             throw new TransformationException(e);
         }
+    }
+
+    @Override
+    public SerializationProperties act(InputStream query, OutputStream output) throws TransformationException {
+        try {
+            XQueryEvaluator e = newEvaluatorOnQuery(query);
+            return evaluate(e, output);
+        } catch (SaxonApiException saxonApiException) {
+            throw new TransformationException(saxonApiException);
+        }
+    }
+
+    private XQueryEvaluator newEvaluatorOnQuery(InputStream query) throws SaxonApiException {
+        return this.getProcessor().newXQueryCompiler().compile(query).load();
+    }
+
+    private SerializationProperties evaluate(XQueryEvaluator e, OutputStream output) throws SaxonApiException {
+        Serializer s = newSerializer(output);
+        e.setDestination(s);
+        e.run();
+        return new SerializationProperties("application/xml", "utf-8");
     }
 }
